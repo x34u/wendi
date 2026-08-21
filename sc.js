fe6020b2151a14d8c7ebb6ad5c749a537a1d5b06
x34u <blsmk.260127081@budiluhur.sch.id>
2026-08-21 11:44:51 +0700

    Create sc.js

---
+++ sc.js
@@ -0,0 +1,15 @@
+let nilai = 95;
+
+switch(true){
+    case nilai >= 90:
+      console.log("Nilai A");
+      break;
+    case nilai >= 80:
+      console.log("Nilai B");
+      break;
+    case nilai >= 70:
+      console.log("Nilai C");
+      break;
+    default:
+      console.log("Nilai D");
+}
